fa9cb750c67c075ba5b38a6eddeeb71fd0e27e3b
Edgar Joaquin Flores <floresedgarjoaquin@gmail.com>
2022-08-09 18:12:53 -0500

    Config database agregado para conexión a mongoDB

---
+++ db/config.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+
+    const dbConnection = async() => {
+        try {
+            await mongoose.connect(process.env.DB_CONNECTION, {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+                useCreateIndex: true,
+                useFindAndModify: false
+            });
+        } catch (error) {
+            console.log(error);
+            throw new Error('Error connecting to database');
+        }
+    }
+
+module.exports = dbConnection;
