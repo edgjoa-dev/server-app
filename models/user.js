745e901706db2dd752b24a74739681d12b0a3858
Edgar Joaquin Flores <floresedgarjoaquin@gmail.com>
2022-08-09 18:59:18 -0500

    Modelo de usuario agregado

---
+++ models/user.js
@@ -0,0 +1,38 @@
+const {model, Schema } = require('mongoose');
+
+const userSchema = Schema({
+    name: {
+        type: String,
+        required: [true, 'Name is required']
+    },
+    email: {
+        type: String,
+        required: [true, 'Email is required'],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required']
+    },
+    img: {
+        type: String,
+    },
+    role: {
+        type: String,
+        required: true,
+        enum: ['USER_ROLE', 'ADMIN_ROLE'],
+    },
+    state: {
+        type: Boolean,
+        default: true
+    },
+    google: {
+        type: Boolean,
+        default: false
+    },
+});
+
+
+
+
+module.exports = model('User', userSchema);
