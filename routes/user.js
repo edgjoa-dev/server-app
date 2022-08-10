8018969e46607634298f6cc6249ed30eab22e346
Edgar Joaquin Flores <floresedgarjoaquin@gmail.com>
2022-08-10 13:54:53 -0500

    validación de rol vs base de datos

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const {Router} = require('express');
 const { check } = require('express-validator');
 const {userGet, postUser, putUser, patchUser, deleteUser } = require('../controllers/user');
 const { validarCampos } = require('../middleware/validar-campos');
+const Role = require('../models/role');
 
 
 const router = Router();
@@ -14,7 +15,14 @@ router.post('/',[
     check('name', 'Name no es válido').not().isEmpty(),
     check('email', 'Email no es válido').isEmail(),
     check('password', 'Password no es válido').isLength({min: 8}),
-    check('role', 'Role no es válido').isIn([ 'ADMIN_ROLE', 'USER_ROLE' ]),
+    check('role').custom( async(role = '') => {
+        const existRole = await Role.findOne({ role });
+        if(!existRole){
+            throw new Error(`El rol registrado: ${role}, no es válido en la base de datos`);
+        }
+    } ),
+    //check('role', 'Role no es válido').isIn([ 'ADMIN_ROLE', 'USER_ROLE' ]),
+
     validarCampos,
 ],postUser)
 
